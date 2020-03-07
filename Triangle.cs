ca435534c5074e9d5f733a1c9bfe3a3b1e925143
Selhon <51131431+Selhon@users.noreply.github.com>
2020-03-08 00:27:25 +0300

    add Side

--- Triangle.cs
+++ Triangle.cs
@@ -19,7 +19,9 @@ public class Triangle
         System.Console.WriteLine($"{x+curr/2} {y+Math.Sqrt(3)/2*curr}");
         System.Console.WriteLine($"{x+curr} {y}");
     }
-    
+    public double Side (){
+        return curr;
+    }
     public void Resize (double c){
         curr=c;
     }
